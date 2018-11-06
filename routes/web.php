ec1b7be56b385c53771ddccaa29082e5179377c9
Cristian <cristian.labadie@gmail.com>
2018-11-06 17:55:59 -0300

    -m#

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,24 @@
 
 Route::get('/', 'TestController@welcome');
 
+Route::middleware(['auth','admin'])->prefix('admin')->group(function () {
+   	Route::get('/products','ProductController@index'); //Listar 
+	Route::get('/products/create','ProductController@create'); //Formulario 
+	Route::post('/products','ProductController@store'); //Registro
+	Route::get('/products/{id}/edit','ProductController@edit'); //Formulario edición del producto 
+	Route::post('/products/{id}/edit','ProductController@update'); //actualizar
+	Route::post('/products/{id}/delete','ProductController@destroy'); //eliminar
 
 
+	Route::get('/products/{id}/images','ImageController@index');
+	Route::post('/products/{id}/images','ImageController@store'); //Registro
+	Route::post('/products/{id}/delete','ImageController@destroy'); //eliminar
+});
 
 Auth::routes();
 
+
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+// CR = Create and Read.
